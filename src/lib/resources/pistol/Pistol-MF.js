305f771945134f30fee82f8246c4bde0ebf27212
sivertsenstian <stian-sivertsen@hotmail.com>
2014-10-26 20:38:11 +0000

    Added test-assets, created a simple skybox and a 'world'. Also implemented very basic fist person movement with camera/object as well as a doom gun that follows the object. Everything implemented directly in the index script for prototyping

---
+++ src/lib/resources/pistol/Pistol-MF.js
@@ -0,0 +1,33 @@
+{
+
+"metadata" : {
+"formatVersion" : 3.1,
+"description"	: "Md 2 model converted from Pistol-MF.md2 using MD2 to json converter."
+},
+
+"scale" : 1.000000,
+
+"materials": [	{
+"DbgColor" : 15658734,
+"DbgIndex" : 0,
+"DbgName" : "md2_material"
+}],
+
+"vertices": [-170.7,-30.1,-26.7,-170.7,21.7,-26.7,-170.7,-31.3,29.7,-170.7,20.5,29.5],
+
+"morphTargets": [
+{ "name": "PISFA0C", "vertices": [-170.7,-30.1,-26.7,-170.7,21.7,-26.7,-170.7,-31.3,29.7,-170.7,20.5,29.5] },
+{ "name": "FISFA1lphaC", "vertices": [-170.1,-30.1,-26.7,-170.1,21.7,-26.7,-170.1,-31.3,29.7,-170.1,20.5,29.5] }
+],
+
+"morphColors": [],
+
+"normals": [],
+
+"colors": [],
+
+"uvs": [[0.125,0.2109375,0.125,0.90625,0.796875,0.2109375,0.796875,0.90625]],
+
+"faces": [8,0,1,2,0,1,2,8,2,1,3,2,1,3]
+
+}
